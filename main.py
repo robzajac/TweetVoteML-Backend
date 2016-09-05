ba0da786fb561a4bb129521f2632ee15dac0043c
Robert Zajac <robzajac97@gmail.com>
2016-09-05 16:37:34 -0400

    added MongoDB

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import pymongo
+from pymongo import MongoClient
+
+MONGO_URL = os.environ.get('MONGOHQ_URL')
+client = MongoClient(MONGO_URL)
+
+# Initialize the database
+db = client.app56172051
+collection = db.users
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+@app.route("/", methods=['GET'])
+def index():
+	users = collection.find()
+	return render_template('index.html', users=users)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
