6647bb20308f505e2cf4b13038f722fb855376af
Robert Zajac <robzajac97@gmail.com>
2016-09-09 18:25:22 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
-import get_users
-from flask import Flask, render_template, request, redirect
-import pymongo
+from flask import Flask, render_template
 from pymongo import MongoClient
 
 MONGO_URL = os.environ.get('MONGOHQ_URL')
@@ -14,10 +12,11 @@ maga_collection = db.maga_users
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def index():
-	users = maga_collection.find()
-	return render_template('index.html', users=users)
+    users = maga_collection.find()
+    return render_template('index.html', users=users)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
