16608d8537bb908c2733e986a40746cad6e42522
Robert Zajac <robzajac97@gmail.com>
2016-09-09 18:32:14 -0400

    refactoring

--- get_friends.py
+++
@@ -1,9 +0,0 @@
-import twitter
-from keys import *
-
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-				  consumer_secret=CONSUMER_SECRET,
-				  access_token_key=ACCESS_TOKEN,
-				  access_token_secret=ACCESS_SECRET,
-				  sleep_on_rate_limit=True)
-
